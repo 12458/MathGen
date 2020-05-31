a5acc9dd373d2129e51f1e73ab4ffe55f827d86d
Sim Shang En <12458@users.noreply.github.com>
2020-05-31 23:29:39 +0800

    Add cleanup

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import glob
 import generator
 from flask import Flask
 from flask import request
@@ -6,7 +7,7 @@ from flask import redirect
 from flask import send_file, current_app as app
 database = "math_alevel_2019.db"
 folder = "static"
-
+path = "static/qn.pdf"
 app = Flask(__name__)
 
 
@@ -15,11 +16,20 @@ def home():
     return render_template("index.html")
 
 
-path = "static/qn.pdf"
 @app.route("/submit", methods=["POST"])
 def submit():
     saved_path = generator.generate(request.form['topic'], database, folder)
     return render_template("pdf.html", url=saved_path)
 
+
+def cleanup(path):
+    import os
+    files = glob.glob(f'{path}/*')
+    for f in files:
+        os.remove(f)
+
+
 if __name__ == "__main__":
+    # Cleanup
+    cleanup(folder)
     app.run(debug=True)
