a7e4d60e5e5959126e4f21c266b1db063291be7c
Sim Shang En <simshangen@gmail.com>
2020-09-05 00:12:28 +0800

    Fix SQL injection vulnerability

--- generator.py
+++ generator.py
@@ -1,51 +1,31 @@
 def generate(criteria, database_location, path_folder):
     try:
         import sqlite3
-        # TO INSTALL : pip install binpacking
-        # pip install latex
         import binpacking
         import random
         from latex import build_pdf
         import hashlib
 
-        # db = sqlite3.connect(':memory:')
-        db = sqlite3.connect(database_location)  # 'math_alevel_2019.db'
-        # db = sqlite3.connect('E:/Dropbox/math_alevel.db')
+        db = sqlite3.connect(database_location)
+
         cursor = db.cursor()
 
-        # criteria = input("Please input your desired topic or skills, separated by commas e.g Differentiation, 1.2.1 \n\n")
         criteria = criteria.split(",")
-
-        sql_statement = '''SELECT question, mark, skill, type, year, paper, question_no
+        sql_statement = '''
+                        SELECT question, mark, skill, type, year, paper, question_no
                         FROM questions_only_wip 
-                        WHERE question IN (SELECT question FROM questions_only_wip ORDER BY RANDOM() LIMIT 100) 
+                        WHERE question IN (SELECT question FROM questions_only_wip ORDER BY RANDOM() LIMIT 100)
+                        AND skill LIKE "%?%" 
                         '''
-        sql_statement = sql_statement + \
-            'AND skill LIKE "%' + criteria[0]+'%"\n'
-
         if criteria[1:] != []:
             for x in criteria[1:]:
-                sql_statement = sql_statement + 'OR skill LIKE "%' + x + '%"\n'
-        # skill=[]
-        # for c in criteria:
+                sql_statement = sql_statement + 'OR skill LIKE "%?%"'
 
-        #'''SELECT question, mark FROM table WHERE id IN (SELECT id FROM questions_only_wip ORDER BY RANDOM() LIMIT 50)'''
-        #'''SELECT question, mark FROM questions_only_wip'''
-        # cursor.execute('''SELECT question, mark
-        #                  FROM questions_only_wip
-        #                  WHERE question
-        #                  IN (SELECT question FROM questions_only_wip ORDER BY RANDOM() LIMIT 50)''')
-        #                   WHERE topic LIKE "%DIFFERENTIATION%" ''')
-        cursor.execute(sql_statement)
+        cursor.execute(sql_statement, criteria)
 
-        # cursor.execute('''SELECT question
-        #                   FROM ALVL_Math_Question_Bank
-        #                   WHERE mark BETWEEN 10 AND 20
-        #                   ''')
         questions = cursor.fetchall()
 
-        questionpack = binpacking.to_constant_volume(
-            questions, 20, weight_pos=1, lower_bound=None, upper_bound=None)
+        questionpack = binpacking.to_constant_volume(questions, 20, weight_pos=1, lower_bound=None, upper_bound=None)
         with open("assets/latex_preamble.txt", "r") as inF:
             lines = inF.readlines()
         # Implement direct latex parsing
@@ -61,30 +41,6 @@ def generate(criteria, database_location, path_folder):
                 "["+str(p[3])+"/"+str(p[4])+"/" + \
                 str(p[5])+"/"+str(p[5]) + "]\n\n"
         latex_code += "\end{enumerate} \end{document}"
-
-        # with open("assets/latex_recommender.txt", "w") as outF:
-        #     outF.writelines(lines)
-
-        #     #outF = open("E:/Dropbox/myOutFile.txt", "w")
-        #     # for p in questions:# write line to output file
-        #     #    currentQ = p[0].split("\n")
-        #     #    finalQ = ""
-        #     #    for i in range(len(currentQ)):
-        #     #        if currentQ[i].strip() != "":
-        #     #            finalQ += currentQ[i]
-        #     #    outF.write(finalQ + "\n\n")
-
-        #     # Write line to output file
-        #     for p in questionpack[random.randint(0, len(questionpack)-1)]:
-        #         currentQ = p[0].split("\n")
-        #         finalQ = ""
-        #         for i in range(len(currentQ)):
-        #             if currentQ[i].strip() != "":
-        #                 finalQ += currentQ[i]
-        #         outF.write(finalQ + "\n\n" + " \hfill{} " +
-        #                    "["+str(p[3])+"/"+str(p[4])+"/"+str(p[5])+"/"+str(p[5]) + "]\n\n")
-        #     outF.write("\end{enumerate} \end{document}")
-
         db.close()
         pdf = build_pdf(latex_code)
         saved_path = f"{path_folder}/{hash(pdf)}.pdf"
